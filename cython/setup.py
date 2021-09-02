601613b6c4518e1afc0592bb8da2fbe30152dd92
Mario Fink <mario.fink@record-evolution.de>
2021-09-02 10:57:21 +0200

    consider platform dependency for building cython extension locally

--- cython/setup.py
+++ cython/setup.py
@@ -1,6 +1,28 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+import os
+import sys
+
+print("building on platform: "+sys.platform)
+os.system("git tag > gittags.log")
+with open ("gittags.log","r") as gt:
+    taglst = gt.readlines()
+os.remove("gittags.log")
+if len(taglst) > 0 :
+    version = taglst[-1].replace('\n','').replace('v','')
+else:
+    version = 'unkown'
+print("building version: "+version)
+
+if sys.platform == "linux" or sys.platform == "darwin" :
+	cmpargs = ['-std=c++17','-Wno-unused-variable']
+	lnkargs = ['-std=c++17','-Wno-unused-variable']
+elif sys.platform == "win32" :
+	cmpargs = ['/EHsc','/std:c++17']
+	lnkargs = []
+else :
+	raise RuntimeError("unknown platform")
 
 extensions = Extension(
     name="imc_termite",
@@ -9,12 +31,12 @@ extensions = Extension(
     # library_dirs=["lib"],
     include_dirs=["lib"],
     language='c++',
-    extra_compile_args=['-std=c++17','-Wno-unused-variable'],
-    extra_link_args=['-std=c++17'],
+    extra_compile_args=cmpargs,
+    extra_link_args=lnkargs,
 )
 
 setup(
-    name="imc_termite",
+    name='imc_termite',
     version='1.2.10',
     description='IMCtermite cython extension',
     author='Record Evolution GmbH',
